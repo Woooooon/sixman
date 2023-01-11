611298880b67f832d1004da182db695637968ce6
DESKTOP-RR7AM05\user1 <odsh507@naver.com>
2023-01-11 12:01:15 +0900

    더미이후 수정중

--- sixman/src/main/java/com/kh/sixman/chat/controller/ChatController.java
+++ sixman/src/main/java/com/kh/sixman/chat/controller/ChatController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,7 +29,7 @@ import lombok.extern.slf4j.Slf4j;
 @ResponseBody
 @Slf4j
 public class ChatController {
-	
+		
 	@Autowired
 	private ChatService chatService;
 	
